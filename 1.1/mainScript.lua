804da2701143856ae75614b172bc5f9fb15d328c
hacker1-c <83634876+hacker1-c@users.noreply.github.com>
2022-07-03 17:10:05 +0700

    Create mainScript.lua

---
+++ 1.1/mainScript.lua
@@ -0,0 +1,21 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Project Nebula", "Midnight")
+local CharacterTab = Window:NewTab("Character")
+local CharacterSection1 = CharacterTab:NewSection("Character")
+CharacterSection1:NewSlider("Speed", "Change your speed", 500, 16, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+CharacterSection1:NewSlider("Jump speed", "Change your jump speed", 500, 16, function(jumpspeed)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = jumpspeed
+end)
+CharacterSection1:NewSlider("Jump Power", "Change your jump power", 120, 50, function(jp)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = jp
+end)
+local OthersTab = Window:NewTab("Others")
+local OthersSection1 = OthersTab:NewSection("Others")
+OthersSection1:NewButton("Infinite Yield FE", "FE Admin commands (op)", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end)
+OthersSection1:NewButton("Bedwars Hack", "Win bedwars game easily", function()
+    loadstring(game:HttpGet("https://s3.us-east-2.amazonaws.com/robloxexploits.net/625456dc5bdb81f6e62a45dd-1654955109088-Bedwars%20script%20GUI%20-%20Robloxexploits.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVASNPL46OESEE36U%2F20220703%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220703T092524Z&X-Amz-Expires=3600&X-Amz-Signature=1c379ab361b712207fc79117de6f9b8dad539ea8544a967b4640d9f64a81f70f&X-Amz-SignedHeaders=host"))()
+end)

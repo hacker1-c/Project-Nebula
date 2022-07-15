12ee6afcc0594eaddfca54191499f30f7f2f71b7
hacker1-c <83634876+hacker1-c@users.noreply.github.com>
2022-07-15 08:49:10 +0700

    Update mainScript.lua

--- 1.0/mainScript.lua
+++ 1.0/mainScript.lua
@@ -1,7 +1,1 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Project Nebula", "Midnight")
-local CharacterTab = Window:NewTab("Character")
-local CharacterSection1 = CharacterTab:NewSection("Character Settings")
-CharacterSection1:NewSlider("Speed", "Change your speed", 500, 30, function(s)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hacker1-c/Project-Nebula/main/1.0/script.lua"))()
